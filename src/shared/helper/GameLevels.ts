1248fbedbbbcdd9f324bd2bb1a7d2e87c5aa1d76
Mario Lubenka <mario.lubenka@googlemail.com>
2017-12-24 13:58:59 +0100

    Adds logo and map

--- src/shared/helper/GameLevels.ts
+++ src/shared/helper/GameLevels.ts
@@ -1,7 +1,9 @@
 import DefaultMap from "../../app/maps/DefaultMap";
 import Level1Map from "../../app/maps/Level1Map";
+import NgHashiMap from "../../app/maps/NgHashiMap";
 
 export default [
   new DefaultMap(),
-  new Level1Map()
+  new Level1Map(),
+  new NgHashiMap()
 ];
