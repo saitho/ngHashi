c47ae0887fb2011549d19d8e319a7b520c444c31
Mario Lubenka <mario.lubenka@googlemail.com>
2017-12-30 21:33:36 +0100

    Use ng-appversion for displaying the app version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-import { version } from '../../build/_versions';
+import { version } from 'ng-appversion/_versions';
 export const environment = {
   production: true,
   version: version
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-import { versionLong } from '../../build/_versions';
+import { versionLong } from 'ng-appversion/_versions';
 export const environment = {
   production: false,
   version: versionLong
